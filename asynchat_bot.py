eea0e573a5f8e086287a201ecd0e56cafb094000
Jason H <jason@noreply>
2014-08-13 04:03:12 -0400

    Cleaned up code.

--- asynchat_bot.py
+++ asynchat_bot.py
@@ -1,30 +1,62 @@
-import asynchat,asyncore,socket
-class asynchat_bot(asynchat.async_chat):
-	def __init__(self, host, port):
-		asynchat.async_chat.__init__(self)
-		self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
-		self.set_terminator('\r\n')
-		self.data=''
-		self.remote=(host,port)
-		self.connect(self.remote)
-	
-	def handle_connect(self):
-		self.push('USER BasicBot 8 %s :BasicBot! http://github.com/jstoker/BasicBot\r\nNICK testbot\r\n' % self.remote[0])
-	
-	def get_data(self):
-		r=self.data
-		self.data=''
-		return r
-	def collect_incoming_data(self, data):
-		self.data+=data
-	def found_terminator(self):
-		data=self.get_data()
-		if data[:4] == 'PING':
-			self.push('PONG %s' % data[5:]+'\r\n')
-		if '001' in data:
-			self.push('JOIN #bots\r\n')
-		if '~hi' in data:
-			self.push('PRIVMSG #bots :hi.\r\n')
+import asynchat
+import asyncore
+import socket
+import time
+
+import config
+
+
+class AsynchatBot(asynchat.async_chat):
+    def __init__(self, host, port):
+        asynchat.async_chat.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_terminator('\r\n')
+        self.data = ''
+        self.remote = (host, port)
+        self.connect(self.remote)
+
+    def handle_connect(self):
+        self.sendLine("USER " + config.nick + " " + config.nick + " " + config.nick + " :" + config.nick)
+        self.sendLine("NICK " + config.nick)
+
+    def get_data(self):
+        r = self.data
+        self.data = ''
+        print("(" + str(time.time()) + ") Recv: " + r)
+        return r
+
+    def collect_incoming_data(self, data):
+        self.data += data
+
+    def sendLine(self, data):
+        print("(" + str(time.time()) + ") Send: " + data)
+        self.push(data + '\r\n')
+
+    def found_terminator(self):
+        data = self.get_data()
+        ds = data.split(" ")
+        if data[:4] == 'PING':
+            self.sendLine('PONG %s' % data[5:])
+        if ds[1] == '376':  # end of MOTD
+            if config.nickserv_password is not None:
+                self.sendLine(
+                    "PRIVMSG NickServ :IDENTIFY " + config.nickserv_username + " + " + config.nickserv_password)
+            for channel, password in config.channels.items():
+                if password is not None:  # channel requires password
+                    self.sendLine('JOIN ' + channel + ' ' + password)
+                else:
+                    self.sendLine('JOIN ' + channel)
+        if ds[1] == "PRIVMSG":
+            if config.channels.has_key(ds[2].lower()):
+                msg = ' '.join(ds[3:])[1:]
+                ms = msg.split(" ")
+                msgl = msg.lower()
+                if msg[0] == config.command_char:
+                    acmd = msg[1:]  # after command char
+                    aspl = acmd.split(" ")
+                    cmd = aspl[0].upper()
+
+
 if __name__ == '__main__':
-	asynchat_bot('127.0.0.1',16667)
-	asyncore.loop()
+    AsynchatBot('irc.freenode.net', 6667)
+    asyncore.loop()
